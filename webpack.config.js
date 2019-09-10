9bc667d437606baa5cb10a6c6bbbe5ffa1850cf1
Yaroslav Osypenko <yaroslav.osypenko@gmail.com>
2019-09-10 23:35:23 +0300

    use node modules

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,7 @@ module.exports = {
   entry: "./src/index.js",
   target: 'node',
   output: {
+    libraryTarget: 'commonjs2',
     path: path.resolve(__dirname, "dist"),
     filename: 'index.js',
   }
